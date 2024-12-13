8ac37270e160e1c7bd803e7e4aef39e1b8a91626
Garrett Harkness <gharkness@sourceallies.com>
2024-12-13 14:35:12 -0600

    fix issues

--- src/test/java/com/gharkness/sdjpajdbc/DaoIntegrationTest.java
+++ src/test/java/com/gharkness/sdjpajdbc/DaoIntegrationTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.dao.TransientDataAccessResourceException;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -98,7 +99,7 @@ public class DaoIntegrationTest {
 
         authorDao.deleteAuthorById(saved.getId());
 
-        assertThrows(EmptyResultDataAccessException.class, () -> {
+        assertThrows(TransientDataAccessResourceException.class, () -> {
             authorDao.getById(saved.getId());
         });
     }
